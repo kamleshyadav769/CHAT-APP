d876f10944090e9da5ee59c8e5a1004ae592cd2a
kamleshyadav769 <yadavkamlesh5251@gmail.com>
2026-06-26 19:49:11 +0530

    Add logging for Resend API key and response data in sendEmail function

--- server/services/sendResendEmail.js
+++ server/services/sendResendEmail.js
@@ -1,14 +1,16 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY ? "FOUND" : "MISSING");
 const sendEmail = async function (email, subject, message) {
     try{
     const data = await resend.emails.send({
-        from: "Chat App <onboarding@resend.dev>",
+        from: 'onboarding@resend.dev', 
         to: email,
         subject: subject,
         html: message
     });
+        console.log("RESEND RESPONSE:", data); // 🔥 ADD THIS
     return data;
 } catch (error) {
     console.error("Error sending email:", error);
