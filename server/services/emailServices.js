c688f3f1f2d3763e9aad5ab5d4ae94bf98e67bb2
kamleshyadav769 <yadavkamlesh5251@gmail.com>
2026-06-26 08:10:46 +0530

    Improve logging in sendOtptoEmail function to track OTP email sending process

--- server/services/emailServices.js
+++ server/services/emailServices.js
@@ -5,8 +5,9 @@ const sendOtptoEmail=async(email,otp)=>{
   
     const subject ='chat verification code';
     const message=EMAIL_OTP_FORMATE.replace(`{otp}`,otp);
-
+console.log("Sending OTP to email:", email);
     await sendEmail(email,subject,message);
+    console.log("OTP sent to email:", email);
     
 }
  
