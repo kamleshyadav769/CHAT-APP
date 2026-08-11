776f66924fb0b8fb21f9207ed65a11142ad6905c
kamleshyadav769 <yadavkamlesh5251@gmail.com>
2026-08-11 21:20:30 +0530

    Fix layoutStore: restore getStorage for localStorage to reset state on page refresh

--- chat-app/src/Store/layoutStore.js
+++ chat-app/src/Store/layoutStore.js
@@ -10,10 +10,10 @@ const useLayoutStore = create(
             setActiveTab: (tab) => set({ activeTab: tab })
         }), {
         name: "layout-storage",
-        //getStorage: () => localStorage,    // i cooment this beacuse i want this to be reset on page refresh
-             partialize: (state) => ({// i use this in place of(getStorage: () => localStorage) because chatlist me conatct already seleted ho jate the page refresh hone par bhi, so i want to reset the selectedContact on page refresh
-                 activeTab: state.activeTab,
-             }),
+        getStorage: () => localStorage,    // i cooment this beacuse i want this to be reset on page refresh
+            //  partialize: (state) => ({// i use this in place of(getStorage: () => localStorage) because chatlist me conatct already seleted ho jate the page refresh hone par bhi, so i want to reset the selectedContact on page refresh
+            //      activeTab: state.activeTab,
+            //  }),
     }
     )
 );
